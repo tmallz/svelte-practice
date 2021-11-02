51cda5e13716f2ab5f2507638b9a0713bf475b4a
Tim O'Malley <tomalley24@gmail.com>
2021-11-02 17:07:34 -0500

    configured tailwind css with svelte

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<nav>
+		<div>
+		  <div>
+			<a href="#">APP LOGO</a>
+	  
+			<!-- Menus -->
+			<div>
+			  <ul>
+				<li>
+				  <a href="#">About</a>
+				</li>
+				<li>
+				  <a href="#">Services</a>
+				</li>
+				<li>
+				  <a href="#">Blog</a>
+				</li>
+				<li>
+				  <a href="#">Contact</a>
+				</li>
+			  </ul>
+			</div>
+	  
+		  </div>
+		</div>
+	  </nav>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+  </style>
