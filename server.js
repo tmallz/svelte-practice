274aadeb1e88d6c9ce2e311e762c680e4cb709e8
Tim O'Malley <tomalley24@gmail.com>
2021-11-09 16:55:54 -0600

    sequilize up and running

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-import { express } from 'express';
-import { cors } from 'cors';
-import { path } from 'path';
-import { sequelize } from './config/connection';
+const routes = require('./routes');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const sequelize = require('./config/connection');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
-app.use(express.static('public'));
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-app.listen(port, () => {
-	console.log(`Server is up at port ${port}`);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log('Now listening'));
