ecfd13d705c8d572753f831f075cb0900a8ef6e6
Tim O'Malley <tomalley@Silverwork-Solutions.com>
2022-01-16 13:31:01 -0600

    initial attempt at getting google maps into svelte

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess';
+import {config} from 'dotenv';
+import replace from '@rollup/plugin-replace';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -80,6 +82,16 @@ export default {
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
 		production && terser(),
+
+		replace({
+			// stringify the object       
+			__myapp: JSON.stringify({
+			  env: {
+				isProd: production,
+				...config().parsed // attached the .env config
+			  }
+			}),
+		  }),
 	],
 	watch: {
 		clearScreen: false,
---
+++ src/homepage.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Map from './Map.svelte';
+	export let ready;
+    const apiKey = _myapp.env.GOOGLE_MAPS_API_KEY;
+</script>
+
+<svelte:head>
+	<script defer async
+	src='https://maps.googleapis.com/maps/api/js?key=&callback=initMap'>
+	</script>
+</svelte:head>
+
+<style>
+:global(body) {
+	padding: 0;
+}
+</style>
+
+{ #if ready }
+<Map></Map>
+{ /if }
