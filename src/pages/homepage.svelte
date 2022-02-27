dc3f1ae449ba5964ca63d8b58a45cb7ad316885d
Tim O'Malley <tomalley24@gmail.com>
2022-02-27 14:31:45 -0600

    got favorite button working for every brewery in the loop

--- src/pages/homepage.svelte
+++ src/pages/homepage.svelte
@@ -17,22 +17,21 @@
  <!-- {#await handleButtonClick}
  <p>waiting</p>	 
  {:then data}  -->
-	<!-- {#each fetchedData as brewery} -->
+	{#each fetchedData as brewery, index (newArray.indexOf(brewery))}
 		<div class="py-2">
 			<div class="relative flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
 				<div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
-					<h3 class="font-black text-gray-800 md:text-3xl text-xl">{breweryName}</h3>
-					<p class="md:text-lg text-gray-500 text-base">{breweryAddress}</p>
-					<a href = "{breweryWebsite}" target ="_blank" class="md:text-lg text-gray-500 text-base">{breweryWebsite}</a>
+					<h3 class="font-black text-gray-800 md:text-3xl text-xl">{brewery.name}</h3>
+					<p class="md:text-lg text-gray-500 text-base">{brewery.street} {brewery.city}, {brewery.state} {brewery.postal_code.split('-')[0]}</p>
+					<a href = "{brewery.website_url}" target ="_blank" class="md:text-lg text-gray-500 text-base">{brewery.website_url}</a>
 				</div>
 				<div class="w-full md:w-1/3 bg-white grid place-items-center">
 					<!-- <img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" /> -->
-					<button on:click={handleSaveToFavorites}>Save to favorites</button>
+					<button on:click={handleSaveToFavorites(index)}>Save to favorites</button>
 				</div>
 			</div>
 		</div>
-	<!-- {/each}
-  -->
+	{/each}
  <!-- {/await} -->
  
 
@@ -45,6 +44,8 @@ let fetchedData = [];
 let breweryName;
 let breweryAddress;
 let breweryWebsite;
+let newArray = [];
+let myDict = {};
 
 var handleButtonClick = async () => {
 	await fetch(`https://api.openbrewerydb.org/breweries?by_city=${name}&per_page=25`)
@@ -52,31 +53,38 @@ var handleButtonClick = async () => {
 		if (res.ok) {
 			res.json().then((data) => {
 				fetchedData = data;
-				console.log(fetchedData);
-				breweryName = fetchedData[0].name;
-				breweryAddress = fetchedData[0].street + ' ' + fetchedData[0].city + ' ' + fetchedData[0].state + ' ' + fetchedData[0].postal_code.split('-')[0];
-				breweryWebsite = fetchedData[0].website_url;
+				myDict = arrayToDict(data);
+				newArray = Object.values(myDict);
+				console.log(newArray);
 				return fetchedData;
 		})
 	}
 });
 }
 
-var handleSaveToFavorites = async () => {
+var arrayToDict = (array) => {
+	let dict = {};
+	let count = 0;
+	array.forEach((item) => {
+		dict[count] = item;
+		count++;
+	})
+	return dict;
+}
 
-	let postBreweryName = breweryName;
-	let postBreweryAddress = breweryAddress;
-	let postBreweryWebsite = breweryWebsite;
+var handleSaveToFavorites = async (index) => {
 
-	console.log(postBreweryName, postBreweryAddress, postBreweryWebsite);
+	breweryName = fetchedData[index].name;
+	breweryAddress = fetchedData[index].street + ' ' + fetchedData[index].city + ' ' + fetchedData[index].state + ' ' + fetchedData[index].postal_code.split('-')[0];
+	breweryWebsite = fetchedData[index].website_url;
 
 	if(breweryName && breweryAddress && breweryWebsite) {
 		const response = await fetch('/api/favorites', {
 			method: 'POST',
 			body: JSON.stringify({
-				name: postBreweryName,
-				address: postBreweryAddress,
-				website: postBreweryWebsite
+				name: breweryName,
+				address: breweryAddress,
+				website: breweryWebsite
 			}),
 			headers: {
 				'Content-Type': 'application/json'
